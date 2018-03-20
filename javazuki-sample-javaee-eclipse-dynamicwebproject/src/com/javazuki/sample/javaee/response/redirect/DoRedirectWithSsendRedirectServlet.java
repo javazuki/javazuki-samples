e93afac5e22a499a8c3b0090cabb26e99c79896c
富岡隆幸 <ttomioka@tmkworks.jp>
2018-03-20 09:25:56 +0900

    追加: SendRedirectによるリダイレクトのサンプルを追加

---
+++ javazuki-sample-javaee-eclipse-dynamicwebproject/src/com/javazuki/sample/javaee/response/redirect/DoRedirectWithSsendRedirectServlet.java
@@ -0,0 +1,34 @@
+package com.javazuki.sample.javaee.response.redirect;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/doRedirectWithSendRedirect")
+public class DoRedirectWithSsendRedirectServlet extends HttpServlet {
+
+  @Override protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    System.out.println("DoRedirectServlet doGet() started");
+
+    // リダイレクトするので無意味だが、確認のため設定する。
+    request.setAttribute("redirectMessage", "Hello Redirect");
+
+    // sendRedirectでリダイレクトすると302となる
+    // 他のステータスでリダイレクトしたい場合は、setStatus() + setHeader()でLocationを設定する
+    // locationの指定は別サーバーならドメインも含めURLを記述する
+    // 同一サーバー内であれば相対パスも可能
+    String contextPath = request.getContextPath();
+    response.sendRedirect(contextPath + "/redirected");
+
+    System.out.println("response.getStatus() :" + response.getStatus());
+    System.out.println("response.getHeader(\"Location\") :" + response.getHeader("Location"));
+
+    // 通常はすぐにreturnする
+    System.out.println("DoRedirectServlet doGet() ended");
+  }
+
+}
